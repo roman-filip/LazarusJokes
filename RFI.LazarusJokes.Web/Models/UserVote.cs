5c48a33fc70354718e8148e04bcc7035e829c975
Roman Filip <roman.filip@embedit.cz>
2017-12-29 18:26:35 +0100

    [v3] - renamed current WEB project

--- RFI.LazarusJokes.Web/Models/UserVote.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RFI.LazarusJokes.Web.Models
-{
-    public class UserVote
-    {
-        public int Vote { get; set; }
-
-        public string UserName { get; set; }
-    }
-}
---
+++ RFI.LazarusJokes.Web_old/Models/JokesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace RFI.LazarusJokes.Web.Models
+{
+    public class JokesViewModel
+    {
+        public JokeSimple NewJoke { get; set; }
+
+        public IEnumerable<Joke> Jokes { get; set; }
+    }
+}
